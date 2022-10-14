b7b3ebb6f10323b30a02a259d72ad05b231b6b3c
Gavin Veitch <gavintveitch@gmail.com>
2022-10-14 16:01:19 -0600

    commit tree 4a597f360e48806cd109183ed42c9938473ae51b

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Toggle from "./Toggle";
 
 function App() {
+
+
   return (
     <div>
       <h3>Toggle</h3>
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+
+  const setState = () =>{
+    return setIsOn((isOn) => !isOn)
+  }
+
+  return <button style={{background : (isOn ? "red" : "white")}} onClick={setState}>{isOn ? 'On' : 'Off'}</button>;
 }
 
 export default Toggle;
